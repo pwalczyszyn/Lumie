04d5c50acd80cf7b34e16c45473347ae5fd6fc12
Alexandre Levacher <levacher.alex@gmail.com>
2018-05-06 15:10:28 +0200

    :white_check_mark: Fix example tests

--- example/controllers/simple-ctrl.spec.js
+++ example/controllers/simple-ctrl.spec.js
@@ -2,18 +2,18 @@ const test = require('ava');
 const request = require('supertest');
 const app = require('../app');
 
-test('[GET] /minimal should pass', async (t) => {
+test('[GET] /simple-ctrl should pass', async (t) => {
     const { body, status } = await request(app)
-    .get('/api/minimal')
+    .get('/api/simple-ctrl')
     .set('Accept', 'application/json');
 
     t.is(status, 200);
     t.is(body.msg, 'I am a minimal implementation');
 });
 
-test('[GET] /minimal/number/12 should return 401', async (t) => {
+test('[GET] /simple-ctrl/number/12 should return 401', async (t) => {
     const { body, status } = await request(app)
-    .get('/api/minimal/number/12')
+    .get('/api/simple-ctrl/number/12')
     .set('Accept', 'application/json');
 
     t.is(status, 401);
