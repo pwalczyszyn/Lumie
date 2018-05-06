c095a6522a0c80fcce03659af58d72cd83df19f7
Alexandre Levacher <levacher.alex@gmail.com>
2018-05-06 12:27:43 +0200

    :fire: Remove unused function

--- src/helpers.js
+++ src/helpers.js
@@ -6,12 +6,7 @@ function capitalize(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-function camelize(str) {
-    return str.replace(/(?:^\w|[A-Z]|\b\w)/g, letter => letter.toUpperCase()).replace(/\s|[-.]+/g, '');
-}
-
 module.exports = {
     JoinPathSlash,
-    capitalize,
-    camelize
+    capitalize
 };
